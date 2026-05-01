d241602f02ddcb8192ee2c4a4c0f9fdd9fddd45a
Sahil <sahi0618@yahoo.in>
2026-05-01 21:19:38 +0530

    Initial commit

---
+++ src/TextArea/TextArea.tsx
@@ -0,0 +1,5 @@
+const TextArea = () => {
+    
+}
+
+export default TextArea;
---
+++ src/TextArea/index.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import TextArea from "./TextArea";
+
+const TextAreaContainer = () => {
+    const [text, setText] = useState("");
+    
+}
+
+export default TextArea;
